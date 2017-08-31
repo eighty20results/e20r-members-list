714f19b10eb1fd9bffe2c7768fe65ccde21bc999
Thomas Sjolshagen <eighty20results@users.noreply.github.com>
2017-08-31 15:29:46 -0600

    BUG FIX: Extra ;

--- class.cache.php
+++ class.cache.php
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace E20R\Utilities;;
+namespace E20R\Utilities;
 
 // Deny direct access to the file
 if ( ! defined( 'ABSPATH' ) && function_exists( 'wp_die' ) ) {
