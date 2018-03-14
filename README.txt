d359e966bb6565c3ee81a8275dde87a3659f8c5d
Thomas Sjolshagen <thomas@eighty20results.com>
2018-03-14 10:31:53 -0600

    ENHANCEMENT: Add build tools & one-click update support tools

---
+++ README.txt
@@ -0,0 +1,26 @@
+=== E20R Enhanced Members List for Paid Memberships Pro ===
+Contributors: eighty20results
+Tags: paid memberships pro, members, memberships, pmpro enhancements, enhanced members list, members list
+Requires at least: 4.9
+Tested up to: 4.9.4
+Stable tag: 2.0
+
+== Description ==
+
+Extensible, sortable & bulk action capable members listing tool for Paid Memberships Pro
+
+== Installation ==
+
+1. Upload the `e20r-members-list` folder to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Changelog ==
+
+
+== 2.0 ==
+
+* Initial public release
+
+
+
+
---
+++ bin/build-plugin.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Build script: Copyright 2016-2018 Eighty/20 Results by Wicked Strong Chicks, LLC
+# New Plugin Framework
+#
+short_name="e20r-members-list"
+server="eighty20results.com"
+include=(class plugin-updates class.${short_name}.php README.txt)
+exclude=(*.yml *.phar composer.* vendor)
+build=(plugin-updates/vendor/*.php)
+plugin_path="${short_name}"
+version=$(egrep "^Version:" ../class.${short_name}.php | sed 's/[[:alpha:]|(|[:space:]|\:]//g' | awk -F- '{printf "%s", $1}')
+metadata="../metadata.json"
+src_path="../"
+dst_path="../build/${plugin_path}"
+kit_path="../build/kits"
+kit_name="${kit_path}/${short_name}-${version}"
+
+echo "Building kit for version ${version}"
+
+mkdir -p ${kit_path}
+mkdir -p ${dst_path}
+
+if [[ -f  ${kit_name} ]]
+then
+    echo "Kit is already present. Cleaning up"
+    rm -rf ${dst_path}
+    rm -f ${kit_name}
+fi
+
+for p in ${include[@]}; do
+	cp -R ${src_path}${p} ${dst_path}
+done
+
+for e in ${exclude[@]}; do
+    find ${dst_path} -type d -iname ${e} -exec rm -rf {} \;
+done
+
+mkdir -p ${dst_path}/plugin-updates/vendor/
+for b in ${build[@]}; do
+    cp ${src_path}${b} ${dst_path}/plugin-updates/vendor/
+done
+
+cd ${dst_path}/..
+zip -r ${kit_name}.zip ${plugin_path}
+ssh ${server} "cd ./www/protected-content/ ; mkdir -p \"${short_name}\""
+scp ${kit_name}.zip ${server}:./www/protected-content/${short_name}/
+scp ${metadata} ${server}:./www/protected-content/${short_name}/
+ssh ${server} "cd ./www/protected-content/ ; ln -sf \"${short_name}\"/\"${short_name}\"-\"${version}\".zip \"${short_name}\".zip"
+rm -rf ${dst_path}
+
+
