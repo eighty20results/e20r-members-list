623487ce62d97d1b54493699d2c37e7aecae3a8e
Thomas Sjolshagen <thomas@eighty20results.com>
2019-11-10 16:57:15 -0700

    BUG FIX: Move the is_local_server() function to the Utilities class

--- licensing/class.licensing.php
+++ licensing/class.licensing.php
@@ -186,7 +186,7 @@ if ( ! class_exists( '\E20R\Utilities\Licensing\Licensing' ) ) {
 				return false;
 			}
 			
-			if ( self::is_local_server() ) {
+			if ( Utilities::is_local_server() ) {
 				$utils->log( "Running on the license server so skipping check (treating as licensed)" );
 				
 				return true;
@@ -747,7 +747,7 @@ if ( ! class_exists( '\E20R\Utilities\Licensing\Licensing' ) ) {
 					$utils->log( "Using new or old version of licensing code..? " . ( self::is_new_version() ? 'New' : 'Old' ) );
 				}
 				
-				self::$ssl_verify = self::is_local_server();
+				self::$ssl_verify = Utilities::is_local_server();
 				
 				if ( E20R_LICENSING_DEBUG ) {
 					$utils->log( "Do we verify the SSL certificate (no if local = home_url())? " . ( self::get_ssl_verify() ? 'Yes' : 'No' ) );
@@ -757,13 +757,5 @@ if ( ! class_exists( '\E20R\Utilities\Licensing\Licensing' ) ) {
 			return self::$instance;
 		}
 		
-		/**
-		 * Is the current server the same as the licensing server
-		 * @return bool
-		 */
-		public static function is_local_server() {
-			return defined( 'E20R_LICENSE_SERVER_URL' ) &&
-			       strpos( home_url(), E20R_LICENSE_SERVER_URL ) === 0;
-		}
 	}
 }
