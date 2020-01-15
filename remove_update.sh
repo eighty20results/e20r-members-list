b4c56e660e263fd0059a1b4d9ffa6628feff1e51
Thomas Sjolshagen <thomas@eighty20results.com>
2020-01-15 09:37:59 -0700

    BUG FIX: input file == output file with grep

--- remove_update.sh
+++ remove_update.sh
@@ -8,6 +8,7 @@ if [[ -f trunk/${BASE_FILE} ]]; then
 
 	if [[ ${has_update} -gt 0 ]]; then
 		echo "ℹ︎ Found unsupported external update script. Removing"
-		grep -v "Utilities::configureUpdateServerV4" trunk/${BASE_FILE} > trunk/${BASE_FILE}
+		grep -v "Utilities::configureUpdateServerV4" trunk/${BASE_FILE} > trunk/${BASE_FILE}.new
+		mv trunk/${BASE_FILE}.new trunk/${BASE_FILE}
 	fi
 fi
