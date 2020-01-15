8dcea1ec3d45edca1fd9735d05ccf06fbd5beb67
Thomas Sjolshagen <thomas@eighty20results.com>
2020-01-15 09:35:11 -0700

    BUG FIX: Bad has_update check

--- remove_update.sh
+++ remove_update.sh
@@ -6,7 +6,7 @@ BASE_FILE="class.e20r-members-list.php"
 if [[ -f trunk/${BASE_FILE} ]]; then
 	has_update=$(grep "Utilities::configureUpdateServerV4" trunk/${BASE_FILE} | wc -l)
 
-	if [[ && ${has_update} -gt 0 ]]; then
+	if [[ ${has_update} -gt 0 ]]; then
 		echo "ℹ︎ Found unsupported external update script. Removing"
 		grep -v "Utilities::configureUpdateServerV4" trunk/${BASE_FILE} > trunk/${BASE_FILE}
 	fi
