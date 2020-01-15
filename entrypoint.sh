df56f304230fe419069a5c6daa7ef519d7ea0bed
Thomas Sjolshagen <thomas@eighty20results.com>
2020-01-15 08:39:31 -0700

    Various updates to allow build & push to wordpress.org

--- entrypoint.sh
+++ entrypoint.sh
@@ -64,8 +64,8 @@ else
 	TMP_DIR="/github/archivetmp"
 	mkdir "$TMP_DIR"
 
-	git config --global user.email "10upbot+github@10up.com"
-	git config --global user.name "10upbot on GitHub"
+	git config --global user.email "thomas@eighty20results.com"
+	git config --global user.name "Eighty/20Results Bot on Github"
 
 	# If there's no .gitattributes file, write a default one into place
 	if [[ ! -e "$GITHUB_WORKSPACE/.gitattributes" ]]; then
