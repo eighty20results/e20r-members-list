c4c0c995d5d87e6530bdd7aa4176109cef5c1677
Thomas Sjolshagen <thomas@eighty20results.com>
2021-01-18 11:40:46 +0100

    BUG FIX: Didn't (always) import the database needed for testing

--- docker-env/import-db.sh
+++ docker-env/import-db.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 PROJECT_NAME='e20r-members-list'
-echo "Importing database for ${PROJECT_NAME}"
-# sleep 30;
-echo $(pwd)
+SLEEP_TIME=15
+echo "Importing database for ${PROJECT_NAME} after a ${SLEEP_TIME} second sleep"
+sleep "${SLEEP_TIME}";
+echo "Import basedir: $(pwd)"
 make wp db import ./mariadb-init/${PROJECT_NAME}.sql
