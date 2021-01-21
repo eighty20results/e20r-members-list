9bc30714e1016db694af063edcd275369af09e01
Thomas Sjolshagen <thomas@eighty20results.com>
2021-01-21 11:30:30 +0100

    BUG FIX: Whitespace clean-up

--- build_env/entrypoint.sh
+++ build_env/entrypoint.sh
@@ -93,7 +93,7 @@ else
 	git config --global user.email "thomas@eighty20results.com"
 	git config --global user.name "Eighty/20Results Bot on Github"
 
-	# If there"s no .gitattributes file, write a default one into place
+	# If there's no .gitattributes file, write a default one into place
 	if [[ ! -e "${GITHUB_WORKSPACE}/.gitattributes" ]]; then
 		cat > "${GITHUB_WORKSPACE}/.gitattributes" <<-EOL
 		/${ASSETS_DIR} export-ignore
