2d6cdfb85d86f52d95e1c73cdccff6826f307523
Thomas Sjolshagen <thomas@eighty20results.com>
2021-01-22 11:51:17 +0100

    BUG FIX: The branch info uses the actual TAG data, so looking for "main" ain't going to work!

--- build_env/entrypoint.sh
+++ build_env/entrypoint.sh
@@ -161,11 +161,11 @@ svn status
 
 echo "➤ Testing that we need to push to Wordpress.org"
 
-if [[ ! -z "${BRANCH}" && "main" == "${BRANCH}" ]]; then
+if [[ -n "${BRANCH}" && "${BRANCH}" =~ ^v[0-9]+\..*[0-9]$ ]]; then
 	echo "➤ In main branch so committing files to Wordpress.org SVN repository..."
 	svn commit -m \"Update to version ${VERSION} from GitHub\" --no-auth-cache --non-interactive  --username \"${SVN_USERNAME}\" --password \"${SVN_PASSWORD}\"
 	echo "✓ Plugin deployed! - Test complete"
 else
-	echo "➤ Not in main branch. Nothing to do"
+	echo "✓ Not in main branch. Nothing to do"
 fi
 
