6cedc20536bd20b066a48ae27c48101d4bd8fc18
Thomas Sjolshagen <thomas@eighty20results.com>
2021-03-20 17:01:13 +0100

    BUG FIX: Added some very basic unit tests for the auto_loader() function

--- tests/wpunit/testcases/E20R_Members_ListTest.php
+++ tests/wpunit/testcases/E20R_Members_ListTest.php
@@ -19,13 +19,11 @@
  *  You can contact us at mailto:info@eighty20results.com
  */
 
-namespace E20R\Members_List\Controller\Test;
+namespace E20R\Members_List\Controller;
 
 use E20R\Members_List\Admin\Members_List;
-use E20R\Members_List\Controller\E20R_Members_List;
 use Codeception\Test\Unit;
 use Brain\Monkey;
-use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Spatie\Snapshots\MatchesSnapshots;
 
 class E20R_Members_ListTest extends Unit {
@@ -50,7 +48,7 @@ class E20R_Members_ListTest extends Unit {
 		Monkey\Functions\when( 'get_current_blog_id' )
 			->justReturn( 1 );
 
-		$GLOBALS['hook_suffix'] = 'e20r-members-list';
+		$GLOBALS['hook_suffix'] = 'page';
 	}
 
 	/**
@@ -80,8 +78,66 @@ class E20R_Members_ListTest extends Unit {
 			->with( array( Members_List::get_instance(), 'load_hooks' ), -1 );
 		Monkey\Actions\expectAdded( 'init' )
 			->with( array( E20R_Members_List::get_instance(), 'load_text_domain' ), 1 );
+		Monkey\Actions\expectAdded( 'e20r_memberslist_process_action' )
+			->with( array( Members_List::get_instance(), 'export_members' ), 10, 3 );
 
 		// Load the class and hooks (make sure the hooks we expect are loaded
 		E20R_Members_List::get_instance()->load_hooks();
+	}
+
+	/**
+	 * Test the happy path for the autoloader
+	 *
+	 * @param string $class_name
+	 * @param bool $expected
+	 *
+	 * @dataProvider fixture_good_class_list
+	 */
+	public function test_auto_loader_success( $class_name, $expected, $message ) {
+
+		$result = E20R_Members_List::auto_loader( $class_name );
+		self::assertEquals( $expected, $result, $message );
+	}
+
+	/**
+	 * Fixture for the auto_loader success tests
+	 *
+	 * @return array[]
+	 */
+	public function fixture_good_class_list() {
+		return array(
+			array( 'E20R\Members_List\Admin\Export_Members', true, 'The Export_Members class is NOT present in the plugin directory?!?' ),
+			array( 'E20R\Members_List\Admin\Members_List_Page', true, 'The Members_List_Page class is NOT present in the plugin directory?!?' ),
+			array( 'E20R\Members_List\Support\Sort_By_Meta', true, 'The Sort_By_Meta class is NOT present in the plugin directory?!?' ),
+		);
+	}
+
+	/**
+	 * Test a failure path for the autoloader
+	 *
+	 * @param string $class_name
+	 * @param bool $expected
+	 *
+	 * @dataProvider fixture_missing_class_list
+	 */
+	public function test_auto_loader_error_returns( $class_name, $expected, $message ) {
+
+		$result = E20R_Members_List::auto_loader( $class_name );
+		self::assertEquals( $expected, $result, $message );
+	}
+
+	/**
+	 * Fixture for the auto loader failure tests
+	 *
+	 * @return array[]
+	 */
+	public function fixture_missing_class_list() {
+		return array(
+			array( 'E20R\Members_List\Controller\E20R_Members_List', false, 'Valid class, but was unexpectedly allowed to load though trying to (re)load' ),
+			array( 'TLS\Members_List\Controller\E20R_Members_List', false, 'Valid class, and unexpectedly in the right namespace (Should be: TLS\\...)' ),
+			array( 'TLS_Does_Not_Exists', false, 'Had the expected namespace prefix ("E20R" - No prefix was specified!)' ),
+			array( 'E20R\\Members_List\\Admin\\My_E20R_Members_List', false, 'My_E20R_Members_List is _NOT_ a valid class in this plugin!' ),
+			array( 'E20\Members_List\Admin\Members_List_Page', false, 'For some reason the typo in the E20R prefix was ignored' ), // Typo
+		);
 	}
 }
