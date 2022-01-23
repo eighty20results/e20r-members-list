4827df0047857f8f9d103ca22552a5070dd24aba
Thomas Sjolshagen <thomas@eighty20results.com>
2022-01-23 11:42:05 +0100

    BUG FIX: Initial stub for the Multiple Memberships Per User add-on

--- src/E20R/members-list/js/e20r-memberslist-page.js
+++ src/E20R/members-list/js/e20r-memberslist-page.js
@@ -17,8 +17,10 @@
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+/* jslint esversion: 6 */
 (function ($) {
     'use strict';
+
     let e20rMembersList_Page = {
         init: function () {
 
@@ -71,9 +73,9 @@
             self.updateListBtn.unbind('click').on('click', function() {
 
 				if ( 'Clear Search' === self.updateListBtn.val() ) {
-                	console.log("We're clearing the search...");
-                    window.console.log(e20rml.url);
-                    window.location.assign(e20rml.url);
+                	window.console.log("We're clearing the search...");
+                    window.console.log(e20rml.url); //jshint ignore:line
+                    window.location.assign(e20rml.url); //jshint ignore:line
                 }
 
                 $('#post-search-input').val(null);
@@ -90,22 +92,22 @@
 
 			self.dataSearchBtn.unbind('click').on('click', function(event) {
 
-				let $search_string = $('#post-search-input').val();
-				let $uri = window.location.toString();
+				let $search_string = $( '#post-search-input' ).val();
+				let $uri		   = window.location.toString();
 
 				// If we have a string in the search box we'll append it to the URL
 				if ($search_string) {
 					event.preventDefault();
 
 					// URL Encode the search string and add or replace it for the URI
-					$uri = self.set_search( $uri, 'find', encodeURIComponent( $search_string ) )
-					console.log( 'New URI is: ' + $uri);
+					$uri = self.set_search( $uri, 'find', encodeURIComponent( $search_string ) );
+					window.console.log( 'New URI should be: ' + $uri );
 
 					// Now we trigger the search
-					window.location = $uri;
+					// window.location = $uri;
 
 					// Clear the search field - Possible FIXME if user's do not want clearing the search field to happen
-					$('#post-search-input').val(null);
+					// $( '#post-search-input' ).val( null );
 				}
 			});
 
@@ -121,7 +123,7 @@
                 window.console.log("New value: " + current_select_info );
 
                 if ( previous_select_info !== current_select_info && '' !== enddate ) {
-                    if ( ! window.confirm(e20rml.lang.clearing_enddate) ) {
+                    if ( ! window.confirm(e20rml.lang.clearing_enddate) ) { //jshint ignore:line
                         return false;
                     }
                 }
@@ -223,18 +225,20 @@
 
                 ev.preventDefault();
                 window.console.log("Export button clicked!");
-                let export_args = {};
-                let inputs = $('.e20r-search-arguments input, .e20r-search-arguments textarea, .e20r-search-arguments select')
+				$("#overlay").fadeIn(300);
+				let inputs = $('.e20r-search-arguments input, .e20r-search-arguments textarea, .e20r-search-arguments select')
                     .not(':input[type=button], :input[type=submit], input[type=reset]');
 
-                self.prepare_export(self, export_args, inputs);
+                let export_args = self.prepare_export(self, inputs);
+				// Open a new tab to trigger the AJAX request for the download
+				self.download_csv('members_list.csv', export_args, ); //jshint ignore:line
             });
         },
 		set_search: function(url, paramName, paramValue) {
 
 			let pattern = new RegExp('\\b('+paramName+'=).*?(&|#|$)');
 
-			if (paramValue == null) {
+			if (paramValue === null) {
 				paramValue = '';
 			}
 
@@ -245,41 +249,116 @@
 			url = url.replace(/[?#]$/,'');
 			return url + (url.indexOf('?')>0 ? '&' : '?') + paramName + '=' + paramValue;
 		},
-		prepare_export: function( self, export_args, inputs ) {
-
-			export_args.action = "e20rml_export_records";
-			export_args._wpnonce = $('#_wpnonce').val();
-			export_args.showDebugTrace = true;
-
+		/**
+		 * Set the request variables based on the members list page
+		 *
+		 * @param self Instance of this object
+		 * @param inputs The HTML inputs
+		 * @returns Object
+		 */
+		prepare_export: function( self, inputs ) {
+			let request_args = {
+				'_wpnonce':  $('#_wpnonce').val(),
+				'showDebugTrace': true,
+				'action': 'e20rml_export_records',
+			};
+			request_args.find = $( '#post-search-input' ).val();
 			inputs.each(function () {
-
 				let input = $(this);
 				let name = input.attr('name');
 				let value = input.val();
-
 				if (false === self.is_empty(value)) {
 					window.console.log(name + " contains " + value);
-
-					export_args[name] = value;
+					request_args[name] = value;
 				}
 			});
 
 			let is_checked = false;
 			let selected_ids = [];
-
 			$('input[name^="member_id"]').each(function () {
-
 				if ($(this).is(':checked')) {
 					is_checked = true;
 					selected_ids.push($(this).val());
 				}
 			});
-
 			if (selected_ids.length > 0) {
-				export_args.member_id = selected_ids;
+				request_args.member_id = selected_ids;
 			}
+			// return self.build_export_url( export_args );
+			return request_args;
+		},
+		/**
+		 * Download the exported .CSV data
+		 * @param filename The file name to use for download
+		 * @param download_args The POST argument(s)
+		 */
+		download_export: function(filename, download_args) {
+			fetch(e20rml.ajax_url, download_args) // jshint ignore:line
+				.then(response => {
+					if (!response.ok) {
+						throw new Error('Request failed.' + response);
+					}
+				})
+				.then(response => response.blob())
+				.then(blob => {
+					const link = window.document.createElement("a");
+					link.href = URL.createObjectURL(blob);
+					link.download = filename;
+					link.click();
+				})
+				.catch(window.console.error);
+		},
+		download_csv: function(file_name, attributes) {
+			let URL = e20rml.ajax_url; // jshint ignore:line
+			window.console.log( 'AJAX URL: ' + URL);
+			window.console.log( attributes );
+			$.ajax({
+				url: URL,
+				cache: false,
+				method: 'POST',
+				data: attributes,
+				accepts: 'text/csv',
+				xhr: function () {
+					var xhr = new XMLHttpRequest();
+					xhr.onreadystatechange = function () {
+						if (xhr.readyState === 2) {
+							if (xhr.status === 200) {
+								xhr.responseType = "blob";
+							} else {
+								xhr.responseType = "text";
+							}
+						}
+					};
+					return xhr;
+				},
+				success: function (data) {
+					//Convert the Byte Data to BLOB object.
+					let blob = new Blob([data], { type: "text/csv" });
 
-			self.submit_export(this, export_args);
+					//Check the Browser type and download the File.
+					let isIE = !!document.documentMode;
+					if (isIE) {
+						window.navigator.msSaveBlob(blob, file_name);
+					} else {
+						let url = window.URL || window.webkitURL;
+						let link = url.createObjectURL(blob);
+						let body = $('body');
+						let a = $("<a />");
+						a.attr("download", file_name);
+						a.attr("href", link);
+						body.append(a);
+						a[0].click();
+						body.remove(a);
+					}
+				},
+				error: function (jqXHR, textStatus, errorThrown) {
+					window.console.log( 'Status: ' + textStatus + ', Error thrown: ' + errorThrown );
+				}
+			}).done(function() {
+				setTimeout(function(){
+					$("#overlay").fadeOut(300);
+				},500);
+			});
 		},
         set_update: function( $element ) {
             let self = this;
@@ -313,7 +392,7 @@
                 window.console.log("Date info? ", date);
 
                 if (Object.prototype.toString.call(date) === "[object Date]") {
-                    $label.text(date.toLocaleDateString(e20rml.locale, self.dateOpts));
+                    $label.text(date.toLocaleDateString(e20rml.locale, self.dateOpts)); //jshint ignore:line
                 }
 
                 test_checked = true;
@@ -325,8 +404,8 @@
 
                 self.bulkActionSelectBottom.val('bulk-update');
                 self.bulkActionSelectTop.val('bulk-update');
-                $('#doaction').val(e20rml.lang.save_btn_text);
-                $('#doaction2').val(e20rml.lang.save_btn_text);
+                $('#doaction').val(e20rml.lang.save_btn_text); //jshint ignore:line
+                $('#doaction2').val(e20rml.lang.save_btn_text); //jshint ignore:line
             }
             /*
             $settings.toggle();
@@ -334,33 +413,24 @@
             */
         },
         /**
-         * Export data to CSV file (from web server)
+         * Build the URL used to export the data
          *
-         * @credit https://gist.github.com/DavidMah/3533415
-         *
-         * @param source
-         * @param data
+         * @param export_attrs The attributes to use for the AJAX URL (export)
          */
-        submit_export: function (source, data) {
+		build_export_url: function (export_attrs) {
+            window.console.log("About to transmit: ", export_attrs);
+			let export_arguments = '';
+			Object.keys(export_attrs).forEach(function(key){
+				let value = export_attrs[key];
+				if ( value instanceof Array) {
+					value = value.join(',');
+				}
+				export_arguments = export_arguments + key + '=' + encodeURI( value ) + '&';
+			});
 
-            window.console.log("About to transmit: ", data);
-
-            let form = $('<form></form>').attr('action', e20rml.url).attr('method', 'post');
-
-            Object.keys(data).forEach(function (key) {
-                let value = data[key];
-
-                if (value instanceof Array) {
-                    value.forEach(function (v) {
-                        form.append($('<input />').attr('type', 'hidden').attr('name', key + "[]").attr('value', v));
-                    });
-                } else {
-                    form.append($('<input />').attr('type', 'hidden').attr('name', key).attr('value', value));
-                }
-            });
-
-            // Send the request.
-            form.appendTo('body').submit().remove();
+			let location = e20rml.ajax_url + '?' + export_arguments; // jshint ignore:line
+			window.console.log( location );
+			return location;
         },
         is_empty: function (data) {
 
