0a0367e67f7677d7bbb638d632bb5fed394e8ddb
Thomas Sjolshagen <thomas@eighty20results.com>
2022-01-28 10:15:34 +0100

    BUG FIX: Add required Namespaces for unit tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -21,6 +21,8 @@
 
 use Codeception\Util\Autoload;
 
+Autoload::addNamespace( 'E20R\\Test\\Unit\\Fixtures', __DIR__ . '/unit/inc/' );
+Autoload::addNamespace( 'E20R\\Utilities', __DIR__ . '/../inc/wp_plugins/00-e20r-utilities/src/E20R/Utilities' );
 Autoload::addNamespace( 'E20R\\Members_List', __DIR__ . '/../src/E20R/members-list/' );
 
 /**
@@ -28,8 +30,8 @@ Autoload::addNamespace( 'E20R\\Members_List', __DIR__ . '/../src/E20R/members-li
  * the constants and class names. You should replace
  * it with something that matches your plugin name.
  */
-// define test environment.
-const PLUGIN_PHPUNIT = true;
+// define test environment
+define( 'PLUGIN_PHPUNIT', true );
 
 if ( ! defined( 'PLUGIN_PATH' ) ) {
 	define( 'PLUGIN_PATH', __DIR__ . '/../src/E20R/members-list/' );
@@ -37,5 +39,5 @@ if ( ! defined( 'PLUGIN_PATH' ) ) {
 
 require_once __DIR__ . '/../inc/autoload.php';
 
-// Load the class autoloader.
+// Load the plugin source(s)
 require_once __DIR__ . '/../class-e20r-members-list.php';
